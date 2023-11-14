fed7fb929a4588ad28125c53330753aea31563d6
Saikat Das <saikatdas0790@gmail.com>
2023-11-14 13:19:19 +0530

    chore: add working setup with axum and agent rs

---
+++ default.nix
@@ -0,0 +1,18 @@
+{}:
+let
+  # rev = "51d906d2341c9e866e48c2efcaac0f2d70bfd43e";
+  # nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/master.tar.gz";
+  pkgs = import nixpkgs { };
+in
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    binaryen
+    cargo-leptos
+    flyctl
+    leptosfmt
+    nodejs_21
+    nodePackages_latest.postcss-cli
+    rustup
+  ];
+}
